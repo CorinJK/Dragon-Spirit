7cd7fa6ad665a73e3ccd61855d71add49a72cdf0
CorinJK <corin16@ya.ru>
2023-07-16 21:54:32 +0300

    Главное меню

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,15 +1,69 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayCurrentLevel1()
+    [SerializeField] private Animator animator;
+    [SerializeField] private GameObject MainMenuScreen;
+    [SerializeField] private GameObject SettingsMenuScreen;
+
+    private Action closeAction;
+
+    private void Start()
     {
-        SceneManager.LoadScene(2);
+        animator.SetTrigger("Show");
+    }
+
+    public void OnStartGame()
+    {
+        closeAction = () => { SceneManager.LoadScene(2); };
+        Close();
     }
 
     public void OpenLevelsList() 
     {
-        SceneManager.LoadScene(1);
+        closeAction = () => { SceneManager.LoadScene(1); };
+        Close();
+    }
+
+    public void OnShowSettings()
+    {
+        closeAction = () => 
+        {
+            MainMenuScreen.SetActive(false);
+            SettingsMenuScreen.SetActive(true);
+        };
+        Close();
+    }
+
+    public void BackToMenu()
+    {
+        SettingsMenuScreen.SetActive(false);
+        MainMenuScreen.SetActive(true);
+        animator.SetTrigger("Show");
+    }
+
+    public void OnExit()
+    {
+        closeAction = () =>
+        {
+            Application.Quit();                                   //выйти из игры в готовой сборке
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;      //выйти из игры внутри редактора Unity
+#endif
+        };
+        Close();
+    }
+
+    public void Close()
+    {
+        animator.SetTrigger("Hide");
+    }
+
+    public void OnCloseAnimationComplete()
+    {
+        closeAction?.Invoke();
     }
 }
